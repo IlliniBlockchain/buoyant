60863cf9c14199ab71706b5ae2c1edec2b1baa7e
Jongwon Park <jwparktom@gmail.com>
2022-03-13 18:54:16 -0500

    Draft of withdrawal

--- program/src/processor/withdraw.rs
+++ program/src/processor/withdraw.rs
@@ -1,72 +1,61 @@
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint::ProgramResult,
-    msg,
-    program_error::ProgramError,
-    program_pack::Pack,
-    pubkey::Pubkey,
-    program::invoke,
-    system_instruction,
+use {
+    crate::{
+        error::SubscriptionError,
+        instruction::SubscriptionInstruction,
+        state::{Counter, Subscription},
+        utils::{
+            assert_msg, check_ata, check_initialized_ata, check_pda, check_program_id, check_signer,
+            check_writable,
+        },
+    },
+    borsh::{BorshDeserialize, BorshSerialize},
+    solana_program::{
+        account_info::{next_account_info, AccountInfo},
+        clock::Clock,
+        entrypoint::ProgramResult,
+        msg,
+        program::{invoke, invoke_signed},
+        program_error::ProgramError,
+        program_pack::Pack,
+        pubkey::Pubkey,
+        system_instruction, system_program,
+        sysvar::{rent, Sysvar},
+    },
+    spl_token::{error::TokenError, state::Account as TokenAccount, state::Mint},
 };
-
-use spl_token::{
-    instruction,
-    state::{Account as TokenAccount},
-};
-
-use borsh::BorshDeserialize;
-
-use crate::{error::{SubscriptionError, EchoError}, utils::assert_msg};
 
 struct Context<'a, 'b: 'a> {
-    payer: &'a AccountInfo<'b>,
+    payer_ai: &'a AccountInfo<'b>,
     payer_token_account: TokenAccount,
     payer_token_account_ai: &'a AccountInfo<'b>,
-    deposit_vault: &'a AccountInfo<'b>,
-    subscription_metadata: &'a AccountInfo<'b>,
-    token_program: &'a AccountInfo<'b>,
+    deposit_vault_ai: &'a AccountInfo<'b>,
+    subscription_metadata_ai: &'a AccountInfo<'b>,
+    token_program_ai: &'a AccountInfo<'b>,
 }
 
 impl<'a, 'b: 'a> Context<'a, 'b> {
     pub fn parse(accounts: &'a [AccountInfo<'b>]) -> Result<Self, ProgramError> {
-        let accounts_iter = &mut accounts.iter();
+        let account_info_iter = &mut accounts.iter();
 
-        let payer = next_account_info(accounts_iter)?;
-        let payer_token_account_ai = next_account_info(accounts_iter)?; // SPL token account
-        let deposit_vault = next_account_info(accounts_iter)?;
-        let subscription_metadata = next_account_info(accounts_iter)?;
-        let token_program = next_account_info(accounts_iter)?;
+        let payer_ai = next_account_info(account_info_iter)?;
+        let payer_token_account_ai = next_account_info(account_info_iter)?; // SPL token account
+        let deposit_vault_ai = next_account_info(account_info_iter)?;
+        let subscription_metadata_ai = next_account_info(account_info_iter)?;
+        let token_program_ai = next_account_info(account_info_iter)?;
 
-        if !payer.is_writable {
-            msg!("Payer must be writable");
-            return Err(EchoError::AccountMustBeWritable.into());
-        }
-
-        if !payer.is_signer {
-            msg!("Payer must be signer");
-            return Err(EchoError::AccountMustBeWritable.into());
-        }
-
-        if !payer_token_account_ai.is_writable {
-            msg!("Payer token account must be writable");
-            return Err(EchoError::AccountMustBeWritable.into());
-        }
-
-        if !deposit_vault.is_writable {
-            msg!("Deposit vault must be writable");
-            return Err(EchoError::AccountMustBeWritable.into());
-        }
+        check_signer(payer_ai);
+        [payer_ai, payer_token_account_ai, deposit_vault_ai].iter().map(|x| check_writable(x));
 
         // Deserialize token account
         let payer_token_account = TokenAccount::unpack_from_slice(&payer_token_account_ai.try_borrow_data()?)?;
 
         Ok(Self {
-            payer,
+            payer_ai,
             payer_token_account,
             payer_token_account_ai,
-            deposit_vault,
-            subscription_metadata,
-            token_program,
+            deposit_vault_ai,
+            subscription_metadata_ai,
+            token_program_ai,
         })
     }
 }
@@ -76,41 +65,40 @@ pub fn process(program_id: &Pubkey, accounts: &[AccountInfo], amount: u64) -> Pr
 
     // Validations
     assert_msg(
-        ctx.payer_token_account.owner != *ctx.payer.key,
+        ctx.payer_token_account.owner == *ctx.payer_ai.key,
         SubscriptionError::InvalidVaultOwner.into(),
         "Invalid vault owner",
     )?;
 
+    // NOTE: do we check for insufficient amount
+    // or do we just pass it to spl_token to deal separately?
 
-    // Transfer to self... ???
-    if ctx.payer.key == ctx.deposit_vault.key {
-        return Ok(());
-    }
-
-    if *ctx.deposit_vault.owner != *ctx.payer.key {
-        return Err(SubscriptionError::InvalidVaultOwner.into());
-    }
-
-    // TODO: include gas
-    if ctx.deposit_vault.lamports() < amount {
-        return Err(SubscriptionError::InsufficientWithdrawBalance.into());
-    }
-
-    // Transfer
-    let instruction = system_instruction::transfer(&ctx.deposit_vault.key, &ctx.payer_token_account.key, amount);
-
-    invoke(
-        &instruction,
-        &[
-            ctx.deposit_vault.clone(),
-            ctx.payer_token_account.clone(),
-        ],
+    msg!("Transferring funds to owner...");
+    // spl token instruction
+    let instruction = &spl_token::instruction::transfer(
+        &spl_token::id(),
+        ctx.deposit_vault_ai.key,
+        ctx.payer_token_account_ai.key,
+        ctx.payer_ai.key,
+        &[],
+        amount,
     )?;
-
-    msg!(
-        "[Buoyant] Withdraw completed. Owner balance: {}",
-        ctx.payer_token_account.lamports()
-    );
+    let withdrawal_seeds = &[
+        b"withdrawal_metadata",
+        ctx.deposit_vault_ai.key.as_ref(),
+        ctx.payer_ai.key.as_ref(),
+        &amount.to_le_bytes(),
+    ];
+    
+    invoke_signed(
+        instruction,
+        &[
+            ctx.deposit_vault_ai.clone(),
+            ctx.payer_token_account_ai.clone(),
+            ctx.payer_ai.clone(),
+        ],
+        &[withdrawal_seeds],
+    )?;
 
     Ok(())
 }
