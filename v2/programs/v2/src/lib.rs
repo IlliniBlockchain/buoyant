053affa738f3f8129d9b61002b63e9f2e32cf912
Alec Chen <alecchendev@gmail.com>
2022-04-03 20:34:28 -0500

    Setup basic functions and state

--- v2/programs/v2/src/lib.rs
+++ v2/programs/v2/src/lib.rs
@@ -1,17 +1,56 @@
 use anchor_lang::prelude::*;
-use solana_program::{
-    entrypoint::ProgramResult
-};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod v2 {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
+        Ok(())
+    }
+    pub fn renew(ctx: Context<Renew>) -> Result<()> {
+        Ok(())
+    }
+    pub fn register(ctx: Context<Register>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize {
+
+}
+
+#[derive(Accounts)]
+pub struct Withdraw {}
+
+#[derive(Accounts)]
+pub struct Renew {}
+
+#[derive(Accounts)]
+pub struct Register {}
+
+#[account]
+#[derive(Default)]
+pub struct Subscription {
+    pub active: bool,
+
+    pub mint: Option<Pubkey>,
+    pub deposit_vault: Pubkey,
+    pub deposit_mint: Pubkey,
+
+    pub payee: Pubkey,
+    pub amount: u64,
+    pub duration: i64, // = UnixTimestamp, although will always be positive
+
+    pub next_renew_time: i64,
+    pub renewal_count: u64,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
+pub struct Counter {
+    pub count: u64,
+}
