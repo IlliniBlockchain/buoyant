d4be8f5cbf24881843964baac9566c95141500e5
Alec Chen <alecchendev@gmail.com>
2022-04-27 11:59:54 -0500

    Add PDA helpers and clean up static test account utilities

---
+++ js/src/state/pda.ts
@@ -0,0 +1,83 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { BN } from "bn.js";
+import { BUOYANT_PROGRAM_ID } from "../constants";
+
+export const getSubscriptionCounterAddress = async (
+  payee: PublicKey,
+  amount: number,
+  duration: number
+): Promise<PublicKey> => {
+  const [subscriptionCounterAddress, subscriptionCounterBump] =
+    await PublicKey.findProgramAddress(
+      [
+        Buffer.from("subscription_counter"),
+        payee.toBuffer(),
+        Buffer.from(new Uint8Array(new BN(amount).toArray("le", 8))),
+        Buffer.from(new Uint8Array(new BN(duration).toArray("le", 8))),
+      ],
+      BUOYANT_PROGRAM_ID
+    );
+  return subscriptionCounterAddress;
+};
+
+export const getSubscriptionAddress = async (
+  payee: PublicKey,
+  amount: number,
+  duration: number,
+  count: number
+): Promise<PublicKey> => {
+  const [subscriptionKey, subscriptionBump] =
+    await PublicKey.findProgramAddress(
+      [
+        Buffer.from("subscription_metadata"),
+        payee.toBuffer(),
+        Buffer.from(new Uint8Array(new BN(amount).toArray("le", 8))),
+        Buffer.from(new Uint8Array(new BN(duration).toArray("le", 8))),
+        Buffer.from(new Uint8Array(new BN(count).toArray("le", 8))),
+      ],
+      BUOYANT_PROGRAM_ID
+    );
+  return subscriptionKey;
+};
+
+export const getSubscriptionCount = async (
+  connection: Connection,
+  payee: PublicKey,
+  amount: number,
+  duration: number
+): Promise<number> => {
+  const subscriptionCounterAddress = await getSubscriptionCounterAddress(
+    payee,
+    amount,
+    duration
+  );
+  const accountInfo = await connection.getAccountInfo(
+    subscriptionCounterAddress
+  );
+  const count =
+    accountInfo == null || accountInfo.data.length == 0
+      ? new BN(0)
+      : new BN(accountInfo.data, (endian = "le"));
+  return count.toNumber();
+};
+
+export const getNewSubscriptionAddress = async (
+  connection: Connection,
+  payee: PublicKey,
+  amount: number,
+  duration: number
+): Promise<PublicKey> => {
+  const subscriptionCount = await getSubscriptionCount(
+    connection,
+    payee,
+    amount,
+    duration
+  );
+  const newSubscriptionAddress = await getSubscriptionAddress(
+    payee,
+    amount,
+    duration,
+    subscriptionCount
+  );
+  return newSubscriptionAddress;
+};
--- js/test/utils.ts
+++ js/test/utils.ts
@@ -1,26 +1,24 @@
 import { Keypair, PublicKey, Connection } from "@solana/web3.js";
-import { createMint, getOrCreateAssociatedTokenAccount } from '@solana/spl-token';
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
 
-let feePayer: Keypair = Keypair.fromSecretKey(
+let feePayer = Keypair.fromSecretKey(
     Uint8Array.from([
-    223,  52, 148,  85,  28, 155, 106,  92,   3,  46, 191,
-    36,  31, 102, 121, 244,  67, 157,  21, 194,   8, 143,
-    89, 172, 121, 103, 130,  92, 140, 151, 101,  13, 199,
-    156, 243, 214, 180, 153, 125,  75, 246, 191, 194,  14,
-    48, 220, 100,  94,  67,  68, 134,  91, 108, 117, 213,
-    40, 205,  29, 230, 207, 220, 238, 187, 103
-    ])
+        44, 152,  71, 225, 202,  62, 208,  47,  99, 134, 230,
+       216, 213, 131, 173, 213, 212,  88,  90, 155,  56,  58,
+        19, 239, 123, 109, 247,  86, 204,  24, 144,  41, 125,
+       186, 203, 180, 161, 170,  86, 140,  91,  88, 166, 154,
+       109,  45, 126,  83, 127,  68, 221, 128, 232,  89, 172,
+       151,  48,  20,  46, 138, 147, 122, 230,  97
+     ])
 );
 const balanceThreshold = 1e5; // arbitrary/temporary
-let mint: PublicKey;
+let mint = new PublicKey("Cj1wxenWnRGb7LwRb1zdDiJStTNrJWtipoA2nHtTrsim");
 const mintDecimals = 9;
 let tokenAccount: PublicKey;
+const tokenBalanceThreshold = 1 * (10 ** (mintDecimals - 4)); // arbitrary/temporary
 
 // gets or creates/airdrops static feePayer to be used across tests
 export const getOrCreateFeePayer = async (connection: Connection): Promise<Keypair> => {
-    if (!feePayer) {
-        feePayer = new Keypair();
-    }
     const balance = await connection.getBalance(feePayer.publicKey);
     if (balance < balanceThreshold) {
         const airdropTx = await connection.requestAirdrop(feePayer.publicKey, 2e9);
@@ -52,5 +50,20 @@ export const getOrCreateTokenAccount = async (connection: Connection): Promise<P
             feePayer.publicKey,
         )).address;
     }
+    await fillTokenAccountIfNeeded(connection);
     return tokenAccount;
+}
+
+export const fillTokenAccountIfNeeded = async (connection: Connection) => {
+    const balance = (await connection.getTokenAccountBalance(tokenAccount)).value.uiAmount * (10 ** mintDecimals);
+    if (balance < tokenBalanceThreshold) {
+        await mintTo(
+            connection,
+            feePayer,
+            mint,
+            tokenAccount,
+            feePayer,
+            2 * (10 ** mintDecimals),
+        );
+    }
 }
